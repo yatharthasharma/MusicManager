2dc45af25c5975293fcd28772c359b1a8b20c008
Sharma <yathartha.sharma@accenture.com>
2017-09-04 10:56:35 +0100

    same framework

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Music
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track x = new Track("Complicated");
+            Track y = new Track("Wish you were here");
+            Track z = new Track("When you're gone");
+            Track xyz = new Track("firework");
+            Track xy = new Track();
+            Track xx = new Track("Why");
+            Artist avril = new Artist("avril");
+            Artist katy = new Artist("KATY");
+            Album al = new Album("avrilsfirst", "avril");
+            Album a3 = new Album("perrybro", "perry");
+            a3.AddTrack(xyz);
+            al.AddTrack(x);
+            al.AddTrack(xx);
+            al.GetTrack();
+            Album a2 = new Album("avrilssecond", "avril");
+            a2.AddTrack(y);
+            a2.AddTrack(z);
+            a2.GetTrack();
+            Console.WriteLine("The album for xyz is: " + xyz.GetAlbum());
+            Console.WriteLine("The artist for xyz is: " + xyz.GetArtist());
+            Console.WriteLine();
+            Console.WriteLine("The album for x is: " + x.GetAlbum());
+            Console.WriteLine("The artist for x is: " + x.GetArtist());
+            Console.WriteLine();
+            Console.WriteLine("The album for xx is: " + xx.GetAlbum());
+            Console.WriteLine("The artist for xx is: " + xx.GetArtist());
+            Console.WriteLine();
+            Console.WriteLine("The album for y is: " + y.GetAlbum());
+            Console.WriteLine("The artist for y is: " + y.GetArtist());
+            Console.WriteLine();
+            Console.WriteLine("The album for z is: " + z.GetAlbum());
+            Console.WriteLine("The artist for z is: " + z.GetArtist());
+        }
+    }
+}
--- MusicBot/Dialogs/RootDialog.cs
+++ MusicBot/Dialogs/RootDialog.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Music;
 
 namespace MusicBot.Dialogs
 {
@@ -18,14 +19,21 @@ namespace MusicBot.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
-            
-            
+            if (activity.Text.Contains("Add track album"))
+            {
+                Track x = new Track("Why");
+                Album xx = new Album("Avril Lavigne 1.0");
+                xx.AddTrack(x);
+                await context.PostAsync($"The track {x} has been added to the album {xx}");
+            }
+            else
+            { 
             // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
             await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
+            }
             context.Wait(MessageReceivedAsync);
         }
     }
