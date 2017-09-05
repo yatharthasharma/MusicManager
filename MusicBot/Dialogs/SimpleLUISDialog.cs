54ad44d91d9ec9dcffe053602467789098fc2fe5
Sharma <yathartha.sharma@accenture.com>
2017-09-05 15:18:54 +0100

    create album functionality

--- MusicBot/Dialogs/SimpleLUISDialog.cs
+++ MusicBot/Dialogs/SimpleLUISDialog.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
@@ -16,19 +13,32 @@ namespace MusicBot
     [Serializable]
     public class SimpleLUISDialog : LuisDialog<object>
     {
-        [LuisIntent("GetTracks")]
-        public async Task GetTracks(IDialogContext context, LuisResult result)
+        [LuisIntent("CreateTrack")]
+        public async Task CreateTracks(IDialogContext context, LuisResult result)
         {
-            await context.PostAsync("I havhashdabshdbahsbdut, sorry!");
+            // create new track
+            string trackName = "";
+            EntityRecommendation rec;
+            if (result.TryFindEntity("trackName", out rec))
+            {
+                trackName = rec.Entity;
+                Track newTrack = new Track(trackName);
+                await context.PostAsync($"The track '{ trackName }' has been created.");
+            }
+            else
+            {
+                await context.PostAsync("Sorry, you did not provide a valid name for the new track.");
+            }
             context.Wait(MessageReceived);
         }
+        // create new artist
         [LuisIntent("CreateArtist")]
         public async Task CreateArtist(IDialogContext context, LuisResult result)
         {
             //await context.PostAsync("CreateArtist intent method start.");     // for testing
-            string artistName;
+            string artistName = "";
             EntityRecommendation rec;
-            if (result.TryFindEntity("ArtistName", out rec))
+            if (result.TryFindEntity("artistName", out rec))
             {
                 artistName = rec.Entity;
                 Artist newArtist = new Artist(artistName);
@@ -41,25 +51,29 @@ namespace MusicBot
             //await context.PostAsync("CreateArtist intent method end.");       // for testing
             context.Wait(MessageReceived);
         }
-
+        // create new album associated with a specific artist
+        // if the artist is present link the newly created album to that specific artist, otherwise create a new artist and link the album to that
         [LuisIntent("CreateAlbum")]
         public async Task CreateAlbum(IDialogContext context, LuisResult result)
         {
             string albumName = "";
-            EntityRecommendation rec;
-            if (result.TryFindEntity("AlbumName", out rec))
+            string artistName = "";
+            EntityRecommendation getArtistName;
+            EntityRecommendation getAlbumName;
+            if (result.TryFindEntity("artistName", out getArtistName) && result.TryFindEntity("albumName", out getAlbumName))
             {
-                albumName = rec.Entity;
-                Album newAlbum = new Album(albumName);
-                await context.PostAsync($"The album '{ albumName }' has been created.");
+                artistName = getArtistName.Entity;
+                albumName = getAlbumName.Entity;
+                Album newAlbum = new Album(albumName, artistName);
+                await context.PostAsync($"New album '{ albumName }' created by the artist ' {artistName} '");
             }
             else
             {
-                await context.PostAsync($"Sorry, you did not provide a valid name for the new album.");
+                await context.PostAsync($"Sorry, you did not provide valid names for the new album and/or artist.");
             }
             context.Wait(MessageReceived);
         }
-
+        // if the statement doesn't match any of the criteria for methods
         [LuisIntent("")]
         public async Task None(IDialogContext context, LuisResult result)
         {
